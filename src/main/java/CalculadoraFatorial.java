2ff68c5d60979f72f4e8b8d9a2a71aa6ddd96da5
TerraCampos <gustav0.silva@protonmail.com>
2023-12-13 11:10:46 -0300

    Commit

--- src/main/java/CalculadoraFatorial.java
+++ src/main/java/CalculadoraFatorial.java
@@ -1,6 +1,10 @@
 public class CalculadoraFatorial {
     public static double fatorial(double valor) throws ArithmeticException {
 
+        if (valor <= 0) {
+            throw new ArithmeticException();
+        }
+
         double radical = Math.sqrt(Math.PI * 2 * valor);
         double exponencial = Math.pow(valor, valor) / Math.pow(Math.E, valor);
 
